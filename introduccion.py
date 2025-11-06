20d92310dad1a7ac2a4e5ccca42be06e655c68cb
Jack625a <ktfut15@gmail.com>
2025-11-05 21:36:24 -0400

    Tipos de datos (Listas)

---
+++ introduccion.py
@@ -0,0 +1,49 @@
+#Introduccion a Python
+#Tipos de datos 
+#1. Tipos de datos Simples (Numericos, cadenas de caracteres, logicos)
+nombre="Kevin Arroyo Montaño"
+precio=150.50
+encender=True
+#2. Tipos de datos Complejos
+#2.1. Listas (Cojunto de datos - mutables) sintaxis= []
+colores=["Rojo","Verde","Azul","Cafe","Rosado"]
+numeros=[334,45,69,1,22.5,354,10]
+lista=["A",15,True]
+#Escritura de datos print()
+#2.1.1. Operaciones de las listas
+#2.1.1.1. Acceso a los elementos de una lista
+print(colores[0]) #El primer elemento de una lista [0]
+print(colores[2])
+print(colores[-1]) #El ultimo elemento de una lista [-1]
+#2.1.1.2. Modificacion de datos de una lista
+colores[1]="Lila"
+print(colores)
+colores[-1]="Celeste"
+print(colores)
+#2.1.1.3. Agregado de datos a una lista
+#2.1.1.3.1. Agregado al final de la lista (append)
+colores.append("Blanco")
+print(colores)
+#2.1.1.3.2. Insertar un elemento en una posicion especifia (insert)
+colores.insert(1,"Negro")
+print(colores)
+#2.1.1.3.3. Agregar varios elementos en la lista (extend)
+colores.extend(["Anaranjado","Amarillo","Plomo","Amarillo"])
+print(colores)
+#2.1.1.4. Eliminacion de elementos en una lista
+#2.1.1.4.1. Eliminacion del primer elemento que coincida con el valor ingresado (remove)
+colores.remove("Amarillo")
+print(colores)
+#2.1.1.4.2. Eliminacion de un elemento en una posicion especifica (pop)
+colores.pop()
+print(colores)
+#2.1.1.4.3. Eliminacion por id en rango definidos del
+del colores[0:2]
+print(colores)
+#2.1.1.4.4. Eliminacion de todos los elementos de la lista (clear)
+colores.clear()
+print(colores)
+
+
+#2.2. Tuplas
+#2.3. Diccionarios
